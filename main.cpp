f37b3727af0280e8db4efa934a4436e1e10926b2
Batuhan <batu0412@gmail.com>
2022-05-15 15:26:42 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "mbed.h"
 #include <chrono>
 #include <cstdio>
+#include <cstdlib>
 
 using namespace std::chrono;
 
@@ -9,43 +10,51 @@ using namespace std::chrono;
 #define SAMPLE_INTERVAL     10ms
 
 AnalogIn ain(PA_0);
-PwmOut led1(LED1);
+DigitalOut led1(LED1);
 Timer t;
+Ticker ticker;
 BufferedSerial serial_port(PA_9, PA_10, BAUD_RATE);
 
-char read_buf = 0;
+char read_buf[32] = {0};
 float write_buf = 0;
 int counter = 0;
+int s_size = 0;
+
+void sampling_led()
+{
+    led1 = !led1;
+}
 
 int main()
 {
     printf("Online \n");
     t.reset();
-    led1.period(1.0f);
-    led1.write(0.5f);
-    led1.suspend();
+    led1 = 0;
     serial_port.set_format(8, BufferedSerial::None, 1);
     while (true) {
         serial_port.read(&read_buf, sizeof(read_buf));
-        if(read_buf == 's')
+        if(read_buf[0] == 's')
         {
-            printf("Sampling \n");
-            led1.resume();
+            serial_port.read(&read_buf, sizeof(read_buf));
+            s_size = atoi(read_buf);
+            printf("Sampling %d\n", s_size);
+            ticker.attach(&sampling_led, 500ms);
             t.start();
-            while(counter++ < SAMPLE_SIZE)
+            while(counter++ < s_size)
             {
                 write_buf = ain.read();
                 serial_port.write(&write_buf, sizeof(write_buf));
                 ThisThread::sleep_for(SAMPLE_INTERVAL);
             }
             t.stop();
-            counter = 0;
-            led1.suspend();
-            t.reset();
             printf("Total Sampled: %d\n", counter - 1);
+            counter = 0;
             printf("The time taken was %llu milliseconds\n", duration_cast<milliseconds>(t.elapsed_time()).count());
+            t.reset();
+            led1 = 0;
+            ticker.detach();
         }
-        if(read_buf == 'd')
+        else if(read_buf[0] == 'd')
         {
             while(counter++ < 20)
             {
