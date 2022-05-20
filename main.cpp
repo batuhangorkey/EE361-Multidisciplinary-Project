a651a3196110a7440e413eb8b08aaf5762501909
Batuhan <batu0412@gmail.com>
2022-05-20 21:22:51 +0300

    update

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "mbed.h"
 #include <chrono>
+#include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 
@@ -11,6 +12,7 @@ using namespace std::chrono;
 
 AnalogIn ain(PA_0);
 DigitalOut led1(LED1);
+InterruptIn but1(D6);
 Timer t;
 Ticker ticker;
 BufferedSerial serial_port(PA_9, PA_10, BAUD_RATE);
@@ -19,6 +21,12 @@ char read_buf[32] = {0};
 float write_buf = 0;
 int counter = 0;
 int s_size = 0;
+bool stop = false;
+
+void stop_sample()
+{
+    stop = true;
+}
 
 void sampling_led()
 {
@@ -28,6 +36,8 @@ void sampling_led()
 int main()
 {
     printf("Online \n");
+    printf("Ref voltage: %d\n", (int) ain.get_reference_voltage());
+    but1.rise(&stop_sample);
     t.reset();
     led1 = 0;
     serial_port.set_format(8, BufferedSerial::None, 1);
@@ -35,9 +45,10 @@ int main()
         serial_port.read(&read_buf, sizeof(read_buf));
         if(read_buf[0] == 's')
         {
+            memset(read_buf, 0, sizeof(read_buf));
             serial_port.read(&read_buf, sizeof(read_buf));
             s_size = atoi(read_buf);
-            printf("Sampling %d\n", s_size);
+            printf("Sampling %d Points...\n", s_size);
             ticker.attach(&sampling_led, 500ms);
             t.start();
             while(counter++ < s_size)
@@ -49,7 +60,8 @@ int main()
             t.stop();
             printf("Total Sampled: %d\n", counter - 1);
             counter = 0;
-            printf("The time taken was %llu milliseconds\n", duration_cast<milliseconds>(t.elapsed_time()).count());
+            printf("The time taken was %llu milliseconds (%llu seconds)\n", duration_cast<milliseconds>(t.elapsed_time()).count(),
+                                                                            duration_cast<seconds>(t.elapsed_time()).count());
             t.reset();
             led1 = 0;
             ticker.detach();
@@ -58,7 +70,27 @@ int main()
         {
             while(counter++ < 20)
             {
-                int raw = ain.read_u16();
+                uint16_t raw = ain.read_u16();
+                printf("Value: %d\n", raw);
+                ThisThread::sleep_for(500ms);
+            }
+            counter = 0;
+        }
+        else if(read_buf[0] == 'r')
+        {
+            stop = false;
+            while(counter++ < 30000)
+            {
+                int raw = -1;
+                if (stop)
+                {
+                    printf("Value: %d\n", raw);
+                    serial_port.write(&raw, sizeof(raw));
+                    printf("Stopped\n");
+                    break;
+                }
+                raw = ain.read_u16();
+                serial_port.write(&raw, sizeof(raw));
                 printf("Value: %d\n", raw);
                 ThisThread::sleep_for(500ms);
             }
