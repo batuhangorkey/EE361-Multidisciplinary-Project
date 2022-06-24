9047e69852c94a9127d088e13312e4696119d350
Batuhan <batu0412@gmail.com>
2022-06-24 13:10:06 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 using namespace std::chrono;
 
 #define BAUD_RATE           115200
-#define SAMPLE_SIZE         1000
 #define SAMPLE_INTERVAL     10ms
 
 AnalogIn ain(PA_0);
@@ -20,10 +19,9 @@ BufferedSerial serial_port(PA_9, PA_10, BAUD_RATE);
 
 char read_buf[32] = {0};
 float write_buf = 0;
-int counter = 0;
-uint32_t s_size = 0;
+uint16_t counter = 0;
+int32_t raw = -1;
 bool stop = false;
-int raw = -1;
 milliseconds sample_interval = SAMPLE_INTERVAL;
 
 void on_but1_rise()
@@ -46,39 +44,7 @@ int main()
     while (true) {
         memset(read_buf, 0, 32);
         serial_port.read(read_buf, sizeof(read_buf));
-        printf("%s\n", read_buf);
-        if(read_buf[0] == 's')
-        {
-            serial_port.read(&s_size, 4);
-            printf("Sampling %d Points...\n", s_size);
-            ticker.attach(&on_tick, 500ms);
-            t.start();
-            while(counter++ < s_size)
-            {
-                write_buf = ain.read();
-                serial_port.write(&write_buf, sizeof(write_buf));
-                ThisThread::sleep_for(SAMPLE_INTERVAL);
-            }
-            t.stop();
-            printf("Total Sampled: %d\n", counter - 1);
-            counter = 0;
-            printf("The time taken was %llu milliseconds (%llu seconds)\n", duration_cast<milliseconds>(t.elapsed_time()).count(),
-                                                                            duration_cast<seconds>(t.elapsed_time()).count());
-            t.reset();
-            led1 = 0;
-            ticker.detach();
-        }
-        else if(read_buf[0] == 'd')
-        {
-            while(counter++ < 20)
-            {
-                uint16_t raw = ain.read_u16();
-                printf("Value: %d\n", raw);
-                ThisThread::sleep_for(500ms);
-            }
-            counter = 0;
-        }
-        else if(read_buf[0] == 'r')
+        if(read_buf[0] == 'r')
         {   
             printf("Waiting\n");
             uint32_t buf = 0;
@@ -90,6 +56,7 @@ int main()
             printf("Sample rate: %lld Hz\n", 1000 / sample_interval.count());
             ticker.attach(&on_tick, 500ms);
             stop = false;
+            t.start();
             while(true)
             {
                 if (stop)
@@ -103,9 +70,15 @@ int main()
                 }
                 raw = ain.read_u16();
                 serial_port.write(&raw, sizeof(raw));
-                printf("Value: %d\n", raw);
+                counter++;
                 ThisThread::sleep_for(sample_interval);
             }
+            t.stop();
+            printf("Total Sampled: %d\n", counter - 1);
+            counter = 0;
+            printf("The time taken was %llu milliseconds (%llu seconds)\n", duration_cast<milliseconds>(t.elapsed_time()).count(),
+                                                                            duration_cast<seconds>(t.elapsed_time()).count());
+            t.reset();
             led1 = 0;
         }
         ThisThread::sleep_for(1s);
